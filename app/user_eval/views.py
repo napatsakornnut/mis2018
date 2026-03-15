37394ef49063c83fa150aa790ef70f6782ae3ffc
Likit Preeyanon <likit.pre@mahidol.ac.th>
2026-03-15 11:14:42 +0700

    added email registration flow for comhealth customers
    added google sign-in for online results view for Mahidol staff

--- app/user_eval/views.py
+++ app/user_eval/views.py
@@ -14,31 +14,31 @@ from app.user_eval.models import EvaluationRecord
 @user_eval.route('/check', methods=['GET', 'POST'])
 def check_evaluate():
     blueprint = request.args.get('blueprint')
-    print(blueprint)
-    _eval_record = EvaluationRecord.query.filter_by(staff_id=current_user.id, blueprint=blueprint).first()
-    if _eval_record:
-        timedelta = arrow.now('Asia/Bangkok').datetime - _eval_record.created_at
-    else:
-        timedelta = None
+    if current_user.is_authenticated:
+        _eval_record = EvaluationRecord.query.filter_by(staff_id=current_user.id, blueprint=blueprint).first()
+        if _eval_record:
+            timedelta = arrow.now('Asia/Bangkok').datetime - _eval_record.created_at
+        else:
+            timedelta = None
 
-    form = EvaluationRecordForm()
-    if request.method == 'GET':
-        if timedelta is None or timedelta.days > 365:
-            return render_template('user_eval/modals/evaluation_form.html',
-                                   form=form, blueprint=blueprint)
-        else:
-            resp = make_response()
-            return resp
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            new_record = EvaluationRecord(staff_id=current_user.id)
-            form.populate_obj(new_record)
-            new_record.blueprint = blueprint
-            new_record.created_at = arrow.now('Asia/Bangkok').datetime
-            db.session.add(new_record)
-            db.session.commit()
-            resp = make_response()
-            resp.headers['HX-Trigger'] = json.dumps({'successAlert': 'ได้รับผลประเมินแล้ว', 'closeModal': ''})
-            return resp
-        else:
-            print(form.errors)
+        form = EvaluationRecordForm()
+        if request.method == 'GET':
+            if timedelta is None or timedelta.days > 365:
+                return render_template('user_eval/modals/evaluation_form.html',
+                                       form=form, blueprint=blueprint)
+            else:
+                resp = make_response()
+                return resp
+        if request.method == 'POST':
+            if form.validate_on_submit():
+                new_record = EvaluationRecord(staff_id=current_user.id)
+                form.populate_obj(new_record)
+                new_record.blueprint = blueprint
+                new_record.created_at = arrow.now('Asia/Bangkok').datetime
+                db.session.add(new_record)
+                db.session.commit()
+                resp = make_response()
+                resp.headers['HX-Trigger'] = json.dumps({'successAlert': 'ได้รับผลประเมินแล้ว', 'closeModal': ''})
+                return resp
+            else:
+                print(form.errors)
